a49195ffdce37d3823584bf64c52254634b8b11f
Danyl <crypto.danzilla@gmail.com>
2024-07-07 14:59:53 -0700

    Add login logic to get the bearer token

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module icbc-road-test-availability-checker
 
 go 1.21.1
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1 // direct
--- main.go
+++ main.go
@@ -13,8 +13,15 @@ import (
 )
 
 const (
-	URL = "https://onlinebusiness.icbc.com/deas-api/v1/web/getAvailableAppointments"
+	LOGIN_URL        = "https://onlinebusiness.icbc.com/deas-api/v1/webLogin/webLogin"
+	APPOINTMENTS_URL = "https://onlinebusiness.icbc.com/deas-api/v1/web/getAvailableAppointments"
 )
+
+type LoginPayload struct {
+	DrvrLastName  string `json:"drvrLastName"`
+	LicenceNumber string `json:"licenceNumber"`
+	Keyword       string `json:"keyword"`
+}
 
 type RequestPayload struct {
 	APosID            int32  `json:"aPosID"`
@@ -55,21 +62,25 @@ func main() {
 		return
 	}
 
-	bearerToken := os.Getenv("APP_BEARER_TOKEN")
 	userAgent := os.Getenv("APP_USER_AGENT")
 	aPosIDStr := os.Getenv("APP_APPOINTMENT_POSITION_ID")
 	lastName := os.Getenv("APP_LAST_NAME")
 	licenseNumber := os.Getenv("APP_LICENSE_NUMBER")
+	keyword := os.Getenv("APP_KEYWORD")
 
 	aPosID, err := strconv.ParseInt(aPosIDStr, 10, 32)
 	if err != nil {
 		panic("aPosID is incorecrly set: " + err.Error())
 	}
-	var bearer = "Bearer " + bearerToken
 
-	appointments, err := getAvailableAppointments(int32(aPosID), lastName, licenseNumber, bearer, userAgent)
+	bearerToken, err := getBearerToken(lastName, licenseNumber, keyword, userAgent)
 	if err != nil {
-		fmt.Println("Internal error: " + err.Error())
+		fmt.Println("Login error: " + err.Error())
+	}
+
+	appointments, err := getAvailableAppointments(int32(aPosID), lastName, licenseNumber, bearerToken, userAgent)
+	if err != nil {
+		fmt.Println("Get appointments error: " + err.Error())
 	}
 
 	res, err := PrettyStruct(appointments)
@@ -78,6 +89,47 @@ func main() {
 	}
 
 	fmt.Println(res)
+}
+
+func getBearerToken(lastName, licenceNumber, keyword, userAgent string) (string, error) {
+	payload := LoginPayload{
+		DrvrLastName:  lastName,
+		LicenceNumber: licenceNumber,
+		Keyword:       keyword,
+	}
+
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal payload: %w", err)
+	}
+
+	req, err := http.NewRequest("PUT", LOGIN_URL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return "", fmt.Errorf("new request failed: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("User-Agent", userAgent)
+	req.Header.Add("Referer", "https://onlinebusiness.icbc.com/webdeas-ui/login;type=driver")
+	req.Header.Add("Cache-Control", "no-cache, no-store")
+
+	client := &http.Client{}
+
+	response, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("unsuccessful request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusUnauthorized {
+		return "", fmt.Errorf("invalid credentials")
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("bad status: %s, status code: %d", response.Status, response.StatusCode)
+	}
+
+	return response.Header.Get("Authorization"), nil
 }
 
 func getAvailableAppointments(aPosID int32, lastName, licenseNumber, bearer, userAgent string) ([]Appointment, error) {
@@ -99,7 +151,7 @@ func getAvailableAppointments(aPosID int32, lastName, licenseNumber, bearer, use
 		return nil, fmt.Errorf("failed to marshal payload: %w", err)
 	}
 
-	req, err := http.NewRequest("POST", URL, bytes.NewBuffer(jsonData))
+	req, err := http.NewRequest("POST", APPOINTMENTS_URL, bytes.NewBuffer(jsonData))
 	if err != nil {
 		return nil, fmt.Errorf("new request failed: %w", err)
 	}
