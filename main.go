0a85c11741dfbc3c54db2501cc02899e83ae5536
Danyl <crypto.danzilla@gmail.com>
2024-07-07 14:35:07 -0700

    Moved main func to the top

--- main.go
+++ main.go
@@ -48,12 +48,36 @@ type Appointment struct {
 	EndTm         string          `json:"endTm"`
 }
 
-func PrettyStruct(data any) (string, error) {
-	val, err := json.MarshalIndent(data, "", "    ")
+func main() {
+	err := godotenv.Load("config.env")
 	if err != nil {
-		return "", err
+		fmt.Println("Error loading .env file")
+		return
 	}
-	return string(val), nil
+
+	bearerToken := os.Getenv("APP_BEARER_TOKEN")
+	userAgent := os.Getenv("APP_USER_AGENT")
+	aPosIDStr := os.Getenv("APP_APPOINTMENT_POSITION_ID")
+	lastName := os.Getenv("APP_LAST_NAME")
+	licenseNumber := os.Getenv("APP_LICENSE_NUMBER")
+
+	aPosID, err := strconv.ParseInt(aPosIDStr, 10, 32)
+	if err != nil {
+		panic("aPosID is incorecrly set: " + err.Error())
+	}
+	var bearer = "Bearer " + bearerToken
+
+	appointments, err := getAvailableAppointments(int32(aPosID), lastName, licenseNumber, bearer, userAgent)
+	if err != nil {
+		fmt.Println("Internal error: " + err.Error())
+	}
+
+	res, err := PrettyStruct(appointments)
+	if err != nil {
+		fmt.Println("PrettyStruct didn't work: " + err.Error())
+	}
+
+	fmt.Println(res)
 }
 
 func getAvailableAppointments(aPosID int32, lastName, licenseNumber, bearer, userAgent string) ([]Appointment, error) {
@@ -109,34 +133,10 @@ func getAvailableAppointments(aPosID int32, lastName, licenseNumber, bearer, use
 	return appointments, nil
 }
 
-func main() {
-	err := godotenv.Load("config.env")
+func PrettyStruct(data any) (string, error) {
+	val, err := json.MarshalIndent(data, "", "    ")
 	if err != nil {
-		fmt.Println("Error loading .env file")
-		return
+		return "", err
 	}
-
-	bearerToken := os.Getenv("APP_BEARER_TOKEN")
-	userAgent := os.Getenv("APP_USER_AGENT")
-	aPosIDStr := os.Getenv("APP_APPOINTMENT_POSITION_ID")
-	lastName := os.Getenv("APP_LAST_NAME")
-	licenseNumber := os.Getenv("APP_LICENSE_NUMBER")
-
-	aPosID, err := strconv.ParseInt(aPosIDStr, 10, 32)
-	if err != nil {
-		panic("aPosID is incorecrly set: " + err.Error())
-	}
-	var bearer = "Bearer " + bearerToken
-
-	appointments, err := getAvailableAppointments(int32(aPosID), lastName, licenseNumber, bearer, userAgent)
-	if err != nil {
-		fmt.Println("Internal error: " + err.Error())
-	}
-
-	res, err := PrettyStruct(appointments)
-	if err != nil {
-		fmt.Println("PrettyStruct didn't work: " + err.Error())
-	}
-
-	fmt.Println(res)
+	return string(val), nil
 }
