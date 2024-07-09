1d97ab99fe6675c35e7f6dfb1a1f9e59517c5af6
Danyl <crypto.danzilla@gmail.com>
2024-07-09 14:08:28 -0700

    Cover successful and server error test cases for auth.

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -7,18 +7,47 @@ import (
 )
 
 func TestGetBearerToken(t *testing.T) {
-	t.Run("Invalid creds", func(t *testing.T) {
-		// Create a mock server
-		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"error": "One or more of your entries is incorrect. Verify that your last name, BC driver’s licence number, and keyword are correct"}`))
-		}))
-		defer mockServer.Close()
 
-		_, err := GetBearerToken(mockServer.URL, "Smith", "12356", "secret", "")
+	testCases := []struct {
+		name           string
+		responseStatus int
+		responseBody   string
+		expectedError  bool
+	}{
+		{
+			name:           "Successful Login",
+			responseStatus: http.StatusOK,
+			responseBody:   `{"Authorization": "Bearer some-token"}`,
+			expectedError:  false,
+		},
+		{
+			name:           "Status Bad Request",
+			responseStatus: http.StatusBadRequest,
+			responseBody:   `{"error": "One or more of your entries is incorrect. Verify that your last name, BC driver’s licence number, and keyword are correct"}`,
+			expectedError:  true,
+		},
+		{
+			name:           "Server Error",
+			responseStatus: http.StatusInternalServerError,
+			responseBody:   `{"error": "server error"}`,
+			expectedError:  true,
+		},
+	}
 
-		if err == nil {
-			t.Errorf("expected error: %v, got: %v", true, err)
-		}
-	})
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Create a mock server
+			mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(tc.responseStatus)
+				w.Write([]byte(tc.responseBody))
+			}))
+			defer mockServer.Close()
+
+			_, err := GetBearerToken(mockServer.URL, "Smith", "12356", "secret", "")
+
+			if (err != nil) != tc.expectedError {
+				t.Errorf("expected error: %v, got: %v", tc.expectedError, err)
+			}
+		})
+	}
 }
