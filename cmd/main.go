d9c7c2556131cc1f4e7fada4cafce06043bf0a33
Danyl <crypto.danzilla@gmail.com>
2024-07-10 11:33:31 -0700

    Move appointment related code to the separate internal/appointment package

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/joho/godotenv"
 
+	"icbc-checker/internal/appointment"
 	"icbc-checker/internal/auth"
 )
 
@@ -18,38 +16,6 @@ const (
 	LOGIN_URL        = "https://onlinebusiness.icbc.com/deas-api/v1/webLogin/webLogin"
 	APPOINTMENTS_URL = "https://onlinebusiness.icbc.com/deas-api/v1/web/getAvailableAppointments"
 )
-
-type RequestPayload struct {
-	APosID            int32  `json:"aPosID"`
-	ExamType          string `json:"examType"`
-	ExamData          string `json:"examDate"`
-	IgnoreReserveTime bool   `json:"ignoreReserveTime"`
-	PrfDaysOfWeek     string `json:"prfDaysOfWeek"`
-	PrfPartsOfDay     string `json:"prfPartsOfDay"`
-	LastName          string `json:"lastName"`
-	LicenseNumber     string `json:"licenseNumber"`
-}
-
-type AppointmentDate struct {
-	Date      string `json:"date"`
-	DayOfWeek string `json:"dayOfWeek"`
-}
-
-type DetailExam struct {
-	Code        string `json:"code"`
-	Description string `json:"description"`
-}
-
-type Appointment struct {
-	AppointmentDt AppointmentDate `json:"appointmentDt"`
-	DlExam        DetailExam      `json:"dlExam"`
-	LemgMsgId     int32           `json:"lemgMsgId"`
-	PosId         int32           `json:"posId"`
-	ResourceId    int32           `json:"resourceId"`
-	Signature     string          `json:"signature"`
-	StartTm       string          `json:"startTm"`
-	EndTm         string          `json:"endTm"`
-}
 
 func main() {
 	err := godotenv.Load("config.env")
@@ -75,7 +41,7 @@ func main() {
 		return
 	}
 
-	appointments, err := getAvailableAppointments(int32(aPosID), lastName, licenseNumber, bearerToken, userAgent)
+	appointments, err := appointment.GetAvailableAppointments(APPOINTMENTS_URL, int32(aPosID), lastName, licenseNumber, bearerToken, userAgent)
 	if err != nil {
 		fmt.Println("Get appointments error: " + err.Error())
 	}
@@ -86,59 +52,6 @@ func main() {
 	}
 
 	fmt.Println(res)
-}
-
-func getAvailableAppointments(aPosID int32, lastName, licenseNumber, bearer, userAgent string) ([]Appointment, error) {
-	tomorrow := time.Now().Add(24 * time.Hour).Format("2006-01-02")
-
-	payload := RequestPayload{
-		APosID:            aPosID,
-		ExamType:          "5-R-1",
-		ExamData:          tomorrow,
-		IgnoreReserveTime: false,
-		PrfDaysOfWeek:     "[0,1,2,3,4,5,6]",
-		PrfPartsOfDay:     "[0,1]",
-		LastName:          lastName,
-		LicenseNumber:     licenseNumber,
-	}
-
-	jsonData, err := json.Marshal(payload)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal payload: %w", err)
-	}
-
-	req, err := http.NewRequest("POST", APPOINTMENTS_URL, bytes.NewBuffer(jsonData))
-	if err != nil {
-		return nil, fmt.Errorf("new request failed: %w", err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Add("Authorization", bearer)
-	req.Header.Add("User-Agent", userAgent)
-
-	client := &http.Client{}
-
-	response, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("unsuccessful request: %w", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode == http.StatusUnauthorized {
-		return nil, fmt.Errorf("invalid credentials")
-	}
-
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
-	}
-
-	var appointments []Appointment
-	err = json.NewDecoder(response.Body).Decode(&appointments)
-	if err != nil {
-		return nil, fmt.Errorf("unsuccessful JSON decode: %w", err)
-	}
-
-	return appointments, nil
 }
 
 func PrettyStruct(data any) (string, error) {
---
+++ internal/appointment/appointment.go
@@ -0,0 +1,94 @@
+package appointment
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type RequestPayload struct {
+	APosID            int32  `json:"aPosID"`
+	ExamType          string `json:"examType"`
+	ExamData          string `json:"examDate"`
+	IgnoreReserveTime bool   `json:"ignoreReserveTime"`
+	PrfDaysOfWeek     string `json:"prfDaysOfWeek"`
+	PrfPartsOfDay     string `json:"prfPartsOfDay"`
+	LastName          string `json:"lastName"`
+	LicenseNumber     string `json:"licenseNumber"`
+}
+
+type AppointmentDate struct {
+	Date      string `json:"date"`
+	DayOfWeek string `json:"dayOfWeek"`
+}
+
+type DetailExam struct {
+	Code        string `json:"code"`
+	Description string `json:"description"`
+}
+
+type Appointment struct {
+	AppointmentDt AppointmentDate `json:"appointmentDt"`
+	DlExam        DetailExam      `json:"dlExam"`
+	LemgMsgId     int32           `json:"lemgMsgId"`
+	PosId         int32           `json:"posId"`
+	ResourceId    int32           `json:"resourceId"`
+	Signature     string          `json:"signature"`
+	StartTm       string          `json:"startTm"`
+	EndTm         string          `json:"endTm"`
+}
+
+func GetAvailableAppointments(appointmentUrl string, aPosID int32, lastName, licenseNumber, bearer, userAgent string) ([]Appointment, error) {
+	tomorrow := time.Now().Add(24 * time.Hour).Format("2006-01-02")
+
+	payload := RequestPayload{
+		APosID:            aPosID,
+		ExamType:          "5-R-1",
+		ExamData:          tomorrow,
+		IgnoreReserveTime: false,
+		PrfDaysOfWeek:     "[0,1,2,3,4,5,6]",
+		PrfPartsOfDay:     "[0,1]",
+		LastName:          lastName,
+		LicenseNumber:     licenseNumber,
+	}
+
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal payload: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", appointmentUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, fmt.Errorf("new request failed: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("Authorization", bearer)
+	req.Header.Add("User-Agent", userAgent)
+
+	client := &http.Client{}
+
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("unsuccessful request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusUnauthorized {
+		return nil, fmt.Errorf("invalid credentials")
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	var appointments []Appointment
+	err = json.NewDecoder(response.Body).Decode(&appointments)
+	if err != nil {
+		return nil, fmt.Errorf("unsuccessful JSON decode: %w", err)
+	}
+
+	return appointments, nil
+}
