8416c9c876d3a4b160529c64c96b755f73192e1d
Danyl <crypto.danzilla@gmail.com>
2024-07-10 23:11:52 -0700

    Extend appointment_test to cover negative case

--- internal/appointment/appointment_test.go
+++ internal/appointment/appointment_test.go
@@ -6,38 +6,73 @@ import (
 )
 
 func TestFindTargetAppointment(t *testing.T) {
-	testResponseBody := `
-		[
-			{
-				"appointmentDt": {
-					"date": "2024-07-23",
-					"dayOfWeek": "Wednesday"
-				},
-				"endTm": "09:55",
-				"lemgMsgId": 1,
-				"posId": 0,
-				"resourceId": 0,
-				"signature": "aaaa",
-				"startTm": "09:20"
+	testCases := []struct {
+		name              string
+		examLastDate      string
+		availableExamDate string
+		responseBody      string
+		expectedFound     bool
+	}{
+		{
+			name:              "Appointment found",
+			examLastDate:      "2024-08-10",
+			availableExamDate: "2024-07-23",
+			responseBody: `
+				[
+					{
+						"appointmentDt": {
+							"date": "2024-07-23",
+							"dayOfWeek": "Wednesday"
+						},
+						"endTm": "09:55",
+						"lemgMsgId": 1,
+						"posId": 0,
+						"resourceId": 0,
+						"signature": "aaaa",
+						"startTm": "09:20"
+					}
+				]`,
+			expectedFound: true,
+		},
+		{
+			name:              "Appointment NOT found",
+			examLastDate:      "2024-08-10",
+			availableExamDate: "2024-08-23", // FIX duplication
+			responseBody: `
+				[
+					{
+						"appointmentDt": {
+							"date": "2024-08-23",
+							"dayOfWeek": "Wednesday"
+						},
+						"endTm": "09:55",
+						"lemgMsgId": 1,
+						"posId": 0,
+						"resourceId": 0,
+						"signature": "aaaa",
+						"startTm": "09:20"
+					}
+				]`,
+			expectedFound: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var appointments []Appointment
+			err := json.Unmarshal([]byte(tc.responseBody), &appointments)
+			if err != nil {
+				t.Errorf("failed to unmarshel test json into []Appointment, err: " + err.Error())
 			}
-		]`
 
-	lastDate := "2024-08-10"
+			availableExamDate, err := FindExamAppointment(appointments, tc.examLastDate)
+			if (err != nil) == tc.expectedFound {
+				t.Errorf("expected appointment present: %v, got: %v", tc.expectedFound, err)
+			}
 
-	t.Run("Taget appointment present", func(t *testing.T) {
-		var appointments []Appointment
-		err := json.Unmarshal([]byte(testResponseBody), &appointments)
-		if err != nil {
-			t.Errorf("failed to unmarshel test json into []Appointment, err: " + err.Error())
-		}
-
-		availableExamDate, err := FindExamAppointment(appointments, lastDate)
-		if err != nil {
-			t.Errorf("expected appointment present: %v, got: %v", true, err)
-		}
-
-		if availableExamDate != "2024-07-23" {
-			t.Errorf("expected date: 2024-07-23, got: %v", err)
-		}
-	})
+			if tc.expectedFound && availableExamDate != tc.availableExamDate {
+				t.Errorf("expected date %v, got: %v", tc.availableExamDate, err)
+			}
+		})
+	}
 }
