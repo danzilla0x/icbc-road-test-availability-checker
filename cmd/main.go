4d675b488038eba8a4082927b255b9e90b45a974
Danyl <crypto.danzilla@gmail.com>
2024-07-14 22:08:44 -0700

    Add checks for env variables for emptiness

--- cmd/main.go
+++ cmd/main.go
@@ -24,12 +24,20 @@ func main() {
 	}
 
 	userAgent := os.Getenv("APP_USER_AGENT")
-	aPosIDStr := os.Getenv("APP_APPOINTMENT_POSITION_ID")
 	examLastDate := os.Getenv("APP_EXAM_LAST_DATE")
 	lastName := os.Getenv("APP_LAST_NAME")
+	if lastName == "" {
+		panic("lastName is empty.")
+	}
 	licenseNumber := os.Getenv("APP_LICENSE_NUMBER")
+	if licenseNumber == "" {
+		panic("licenseNumber is empty.")
+	}
 	keyword := os.Getenv("APP_KEYWORD")
-
+	if keyword == "" {
+		panic("keyword is empty.")
+	}
+	aPosIDStr := os.Getenv("APP_APPOINTMENT_POSITION_ID")
 	aPosID, err := strconv.ParseInt(aPosIDStr, 10, 32)
 	if err != nil {
 		panic("aPosID is incorecrly set: " + err.Error())
